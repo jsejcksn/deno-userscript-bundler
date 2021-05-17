7b640f108643597c98b6b0396eeb8fe0a3794790
Jesse Jackson <jsejcksn@users.noreply.github.com>
2021-05-17 18:47:40 -0500

    Update deno std deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export * as bundler from './bundler/mod.ts';
-export * as flags from 'https://deno.land/std@0.95.0/flags/mod.ts';
-export * as path from 'https://deno.land/std@0.95.0/path/mod.ts';
-export {ensureDir} from 'https://deno.land/std@0.95.0/fs/ensure_dir.ts';
-export {readLines} from 'https://deno.land/std@0.95.0/io/mod.ts';
-export {Status} from 'https://deno.land/std@0.95.0/http/http_status.ts';
+export * as flags from 'https://deno.land/std@0.97.0/flags/mod.ts';
+export * as path from 'https://deno.land/std@0.97.0/path/mod.ts';
+export {ensureDir} from 'https://deno.land/std@0.97.0/fs/ensure_dir.ts';
+export {readLines} from 'https://deno.land/std@0.97.0/io/mod.ts';
+export {Status} from 'https://deno.land/std@0.97.0/http/http_status.ts';
