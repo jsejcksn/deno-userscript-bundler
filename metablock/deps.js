dc41291f47613e839eadad6fa557fc5c5343a1c3
Jesse Jackson <jsejcksn@users.noreply.github.com>
2021-06-14 14:08:21 -0500

    fix: Update dependency: semver
    Fix compiler type error in Deno 1.11.0

--- metablock/deps.js
+++ metablock/deps.js
@@ -1,7 +1,7 @@
 import {parse} from 'https://deno.land/std@0.95.0/encoding/yaml.ts';
 
 export * as path from 'https://deno.land/std@0.95.0/path/mod.ts';
-export * as semver from 'https://deno.land/x/semver@v1.3.0/mod.ts';
+export * as semver from 'https://deno.land/x/semver@v1.4.0/mod.ts';
 export {existsSync} from 'https://deno.land/std@0.95.0/fs/exists.ts';
 export {isUri} from './valid-url@1.0.9.js';
 
